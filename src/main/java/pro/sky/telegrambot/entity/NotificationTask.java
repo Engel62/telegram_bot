5cb960191e3331d0e8f8a5ad3094626bd0c96ab7
Engel62 <sr.engel@bk.ru>
2023-07-02 22:11:15 +0300

    entity

---
+++ src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
@@ -0,0 +1,51 @@
+package pro.sky.telegrambot.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notification_task")
+public class NotificationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String message;
+    @Column(name = "chat_id", nullable = false)
+    private long chat_id;
+    @Column(name = "notification_date_time", nullable = false)
+    private LocalDateTime notificationDateTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getChat_id() {
+        return chat_id;
+    }
+
+    public void setChat_id(long chat_id) {
+        this.chat_id = chat_id;
+    }
+
+    public LocalDateTime getNotificationDateTime() {
+        return notificationDateTime;
+    }
+
+    public void setNotificationDateTime(LocalDateTime notificationDateTime) {
+        this.notificationDateTime = notificationDateTime;
+    }
+}
